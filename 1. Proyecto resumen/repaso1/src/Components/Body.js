6a9fb43211741eda3a172fb7441813c52d92d2fc
Agustin Vanetta <agvanetta@gmail.com>
2022-03-19 14:02:28 -0300

    reactProyect1

--- 1. Proyecto resumen/repaso1/src/Components/Body.js
+++ 1. Proyecto resumen/repaso1/src/Components/Body.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
-
+import styles from "../Components/button.module.css"
 export class Body extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+       style:"Main",
+       textButton:"Change Style"
+    }
+    this.ChangeStyle=this.ChangeStyle.bind(this)
+  }
+  ChangeStyle(){
+    this.setState({style:"theme",textButton:""})
+  }
   render() {
     return (
-      <div className="Main">
+      <div className={this.state.style} >
           <img className="img" alt="stock" src={this.props.url_image} height="250px" width={300} />
           <h1> {this.props.company} </h1>
           <ul>
@@ -12,9 +23,10 @@ export class Body extends Component {
               <li className="list">Country : {this.props.country}</li>
               <li className="list">Ticker : {this.props.ticker}</li>
           </ul>
+          <button className={styles.styleButton} style={{backgroundColor:"orange"}} onClick={this.ChangeStyle}>{this.state.textButton}</button>
       </div>
     )
   }
 }
-
+// onClick={()=>this.ChangeStyle()}
 export default Body
