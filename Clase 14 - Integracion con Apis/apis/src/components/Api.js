8fb9d781759df76cacc2beaa385ba04d1ab41d1f
Agustin Vanetta <agvanetta@gmail.com>
2022-03-31 10:53:46 -0300

    semana5

---
+++ Clase 14 - Integracion con Apis/apis/src/components/Api.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+function Pokemon(props){
+    return(
+    <figure>
+        <img src={props.avatar} alt={props.name}/>
+        <figcaption>{props.name}</figcaption>
+    </figure> );
+}
+
+export default class AjaxApis extends Component{
+    state={
+        pokemons:[],
+    }
+
+    componentDidMount(){
+        let url="https://pokeapi.co/api/v2/pokemon/";
+        fetch(url)
+            .then(res=>res.json())
+            .then(json => {
+                console.log(json)
+            })
+    }
+
+    render() {
+        return (
+            <>
+            <h2>Peticiones Asincronas en Componentes de Clase</h2>
+            </>
+        );
+    }
+}
